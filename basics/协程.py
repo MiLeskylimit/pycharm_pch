636fb5f94d168135bd805ac50b7e2f7d77c4d107
MILE <510318967@qq.com>
2021-01-19 17:53:33 +0800

    20200119

--- basics/协程.py
+++ basics/协程.py
@@ -29,4 +29,71 @@ async def main(urls):
         await task
 
 asyncio.run(main(['url_1', 'url_2', 'url_3', 'url_4']))
-0
+
+# 协程中的任务限时、异常处理
+import asyncio
+
+async def worker_1():
+    await asyncio.sleep(1)
+    return 1
+
+async def worker_2():
+    await asyncio.sleep(2)
+    return 2 / 0
+
+async def worker_3():
+    await asyncio.sleep(3)
+    return 3
+
+async def main():
+    task_1 = asyncio.create_task(worker_1())
+    task_2 = asyncio.create_task(worker_2())
+    task_3 = asyncio.create_task(worker_3())
+
+    await asyncio.sleep(2)
+    task_3.cancel()
+
+    # return_exceptions=True不设置就会在任务层抛错，导致任务执行不完
+    res = await asyncio.gather(task_1, task_2, task_3, return_exceptions=True)
+    print(res)
+
+asyncio.run(main())
+
+# 协程例子01
+import asyncio
+import random
+
+async def consumer(queue, id):
+    while True:
+        val = await queue.get()
+        print('{} get a val: {}'.format(id, val))
+        await asyncio.sleep(1)
+
+async def producer(queue, id):
+    for i in range(5):
+        val = random.randint(1, 10)
+        await queue.put(val)
+        print('{} put a val: {}'.format(id, val))
+        await asyncio.sleep(1)
+
+async def main():
+    queue = asyncio.Queue()
+
+    consumer_1 = asyncio.create_task(consumer(queue, 'consumer_1'))
+    consumer_2 = asyncio.create_task(consumer(queue, 'consumer_2'))
+
+    producer_1 = asyncio.create_task(producer(queue, 'producer_1'))
+    producer_2 = asyncio.create_task(producer(queue, 'producer_2'))
+
+    await asyncio.sleep(10)
+    consumer_1.cancel()
+    consumer_2.cancel()
+
+    await asyncio.gather(consumer_1, consumer_2, producer_1, producer_2, return_exceptions=True)
+
+asyncio.run(main())
+
+
+
+
+
